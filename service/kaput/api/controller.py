0eecaa6bde0ba28af6539e3db53b212c7c4af568
Tyler Treat <ttreat31@gmail.com>
2014-02-22 17:58:22 -0600

    Implement commit aggregation

--- service/kaput/api/controller.py
+++ service/kaput/api/controller.py
@@ -24,10 +24,18 @@ def process_git_push():
     repo = Repository.get_by_id('github_%s' % repo_id)
 
     if not repo:
+        logging.error('Repo github_%s not found' % repo_id)
         return 'Repo github_%s not found' % repo_id, 404
 
     if not repo.enabled:
+        logging.debug('Repo github_%s disabled' % repo_id)
         return 'Repo github_%s disabled' % repo_id, 200
+
+    owner = repo.owner.get()
+
+    if not owner:
+        logging.error('Owner not found for repo github_%s' % repo_id)
+        return 'Owner not found for repo github_%s' % repo_id
 
     # TODO: fan out to taskqueue.
 
--- service/kaput/repository.py
+++ service/kaput/repository.py
@@ -1,6 +1,15 @@
+import re
+
 from google.appengine.ext import ndb
 
+from furious import context
+from furious.async import defaults
+
+from kaput.services import gh
 from kaput.user import User
+
+
+COMMIT_QUEUE = 'commit-aggregation'
 
 
 class Repository(ndb.Model):
@@ -20,8 +29,6 @@ class Commit(ndb.Model):
     committer_name = ndb.StringProperty(indexed=False)
     committer_email = ndb.StringProperty(indexed=False)
     committer_date = ndb.DateTimeProperty(indexed=False)
-    author = ndb.JsonProperty(indexed=False, required=False)
-    committer = ndb.JsonProperty(indexed=False, required=False)
     message = ndb.TextProperty(indexed=False, required=False)
 
 
@@ -31,4 +38,55 @@ class CommitChunk(ndb.Model):
     filename = ndb.StringProperty()
     start_line = ndb.IntegerProperty()
     line_count = ndb.IntegerProperty(default=0)
+
+
+def process_repo_push(repo, owner, push_data):
+
+    with context.new() as ctx:
+        for commit in push_data['commits']:
+            ctx.add(
+                target=process_commit,
+                args=(repo.key.id(), commit['id'], owner.github_access_token))
+
+
+@defaults(queue=COMMIT_QUEUE)
+def process_commit(repo_id, commit_id, owner_token):
+    # TODO: error handling/retries
+    repo = Repository.get_by_id(repo_id)
+    github = gh.client(owner_token)
+
+    gh_commit = github.get_repo(repo.name).get_commit(commit_id)
+    author = gh_commit.commit.author
+    committer = gh_commit.commit.committer
+
+    commit = Commit(
+        repo=repo.key, sha=commit_id, author_name=author.name,
+        author_email=author.email, author_date=author.date,
+        committer_name=committer.name, committer_email=committer.email,
+        committer_date=committer.date, message=gh_commit.commit.message)
+
+    chunks = []
+
+    for commit_file in commit.files:
+        chunks.extend(_parse_chunks(commit_file))
+
+    ndb.put_multi([commit] + chunks)
+
+
+def _parse_chunks(commit, commit_file):
+    patch = commit_file.patch
+
+    chunks = []
+
+    for patch in re.findall('@@(.*)@@', patch, re.IGNORECASE):
+        _, after = patch.strip().split(' ')
+
+        start_line, line_count = after.split(',')
+
+        chunks.append(CommitChunk(commit=commit.key,
+                                  filename=commit_file.filename,
+                                  start_line=start_line,
+                                  line_count=line_count))
+
+    return chunks
 
