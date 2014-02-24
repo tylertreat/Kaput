ea2ee271067425424a68112da5b1b1aa4bfc7f04
Tyler Treat <ttreat31@gmail.com>
2014-02-23 18:40:37 -0600

    Fix commit data aggregation

--- service/kaput/repository.py
+++ service/kaput/repository.py
@@ -24,9 +24,11 @@ class Commit(ndb.Model):
 
     repo = ndb.KeyProperty(kind=Repository)
     sha = ndb.StringProperty(indexed=False)
+    author = ndb.KeyProperty(kind=User, required=False)
     author_name = ndb.StringProperty(indexed=False)
     author_email = ndb.StringProperty(indexed=False)
     author_date = ndb.DateTimeProperty(indexed=False)
+    committer = ndb.KeyProperty(kind=User, required=False)
     committer_name = ndb.StringProperty(indexed=False)
     committer_email = ndb.StringProperty(indexed=False)
     committer_date = ndb.DateTimeProperty(indexed=False)
@@ -39,6 +41,7 @@ class CommitChunk(ndb.Model):
     filename = ndb.StringProperty()
     start_line = ndb.IntegerProperty()
     line_count = ndb.IntegerProperty(default=0)
+    timestamp = ndb.DateTimeProperty()
 
 
 def process_repo_push(repo, owner, push_data):
@@ -59,25 +62,33 @@ def process_commit(repo_id, commit_id, owner_token):
     # TODO: error handling/retries
     repo = Repository.get_by_id(repo_id)
     github = gh.client(owner_token)
+    user = github.get_user()
 
-    gh_commit = github.get_repo(repo.name).get_commit(commit_id)
+    gh_commit = user.get_repo(repo.name).get_commit(commit_id)
     author = gh_commit.commit.author
     committer = gh_commit.commit.committer
 
+    author_user = User.get_by_id('github_%s' % gh_commit.author.id)
+    committer_user = User.get_by_id('github_%s' % gh_commit.committer.id)
+
     commit = Commit(
-        repo=repo.key, sha=commit_id, author_name=author.name,
+        id=commit_id, parent=repo.key, repo=repo.key, sha=commit_id,
+        author=author_user.key, author_name=author.name,
         author_email=author.email, author_date=author.date,
-        committer_name=committer.name, committer_email=committer.email,
-        committer_date=committer.date, message=gh_commit.commit.message)
+        committer=committer_user.key, committer_name=committer.name,
+        committer_email=committer.email, committer_date=committer.date,
+        message=gh_commit.commit.message)
+
+    logging.debug('Saving commit %s' % commit_id)
+    commit.put()
 
     chunks = []
 
-    for commit_file in commit.files:
+    for commit_file in gh_commit.files:
         chunks.extend(_parse_chunks(commit, commit_file))
 
-    logging.debug('Saving commit %s with %s chunks' % (commit_id, len(chunks)))
-
-    ndb.put_multi([commit] + chunks)
+    logging.debug('Saving %d commit chunks' % len(chunks))
+    ndb.put_multi(chunks)
 
 
 def _parse_chunks(commit, commit_file):
@@ -90,10 +101,11 @@ def _parse_chunks(commit, commit_file):
 
         start_line, line_count = after.split(',')
 
-        chunks.append(CommitChunk(commit=commit.key,
+        chunks.append(CommitChunk(parent=commit.key, commit=commit.key,
                                   filename=commit_file.filename,
-                                  start_line=start_line,
-                                  line_count=line_count))
+                                  start_line=int(start_line),
+                                  line_count=int(line_count),
+                                  timestamp=commit.author_date))
 
     return chunks
 
